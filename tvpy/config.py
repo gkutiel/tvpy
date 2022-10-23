67dcf916d8023cd11bfd00068cc0d8d3ebb3cbdf
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-10-23 17:20:51 +0300

    config

--- tvpy/config.py
+++ tvpy/config.py
@@ -1,2 +1,16 @@
+
 VERSION = 0.3
 POSTER_WIDTH = 160
+
+
+class format:
+    resolution = '720p'
+    encoder = 'NTB'
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    import PTN
+    p = PTN.parse('Star.Wars.Andor.S01E07.Announcement.1080p.DSNP.WEB-DL.DDP5.1.1.H.264-NTb.zip')
+    pprint(p)
--- tvpy/tv_subt.py
+++ tvpy/tv_subt.py
@@ -1,6 +1,11 @@
+import zipfile
+from pathlib import Path
+
 import requests
 from rich.pretty import pprint
+from rich.status import Status
 
+from tvpy.config import format
 from tvpy.tv_info import load_tvpy
 
 
@@ -8,7 +13,16 @@ def get(imdb_id, season, episode):
     url = f'https://wizdom.xyz/api/releases/{imdb_id}'
     res = requests.get(url).json()
     subs = res['subs']
-    return subs[f'{season}'][f'{episode}'][0]
+    subs = subs[f'{season}'][f'{episode}']
+    return subs
+
+
+def select_sub(subs):
+    for sub in subs:
+        if sub['release_group'] == format.encoder:
+            return sub
+
+    return subs[0]
 
 
 def down_sub(sub_id, out_zip):
@@ -18,10 +32,18 @@ def down_sub(sub_id, out_zip):
 
 
 def tv_subt(folder):
-    info = load_tvpy(folder)
-    imdb_id = info['imdb_id']
-    sub = get(imdb_id, 1, 7)
-    pprint(sub)
-    sub_version = sub['version']
-    sub_id = sub['id']
-    down_sub(sub_id, f'{sub_version}.zip')
+    with Status('[red]Searching...') as status:
+        info = load_tvpy(folder)
+        imdb_id = info['imdb_id']
+        subs = get(imdb_id, 1, 7)
+        sub = select_sub(subs)
+        sub_version = sub['version']
+        sub_id = sub['id']
+
+        status.update('[orange1]Downloading...')
+        zip_file = Path(folder) / f'{sub_version}.zip'
+        down_sub(sub_id, zip_file)
+
+        status.update('[green]Extracting...')
+        with zipfile.ZipFile(zip_file) as z:
+            z.extractall()
