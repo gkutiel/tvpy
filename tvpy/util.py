22d7ff85ed9d2cf62d22b408f4ffe7b74a0f9db6
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-10-25 19:47:03 +0300

    util

--- tvpy/util.py
+++ tvpy/util.py
@@ -2,8 +2,6 @@ from itertools import chain
 from pathlib import Path
 
 
-
-
 def load_key():
     with open('key.txt') as f:
         return f.read().strip()
@@ -13,9 +11,12 @@ def folders(root):
     return [x for x in Path(root).iterdir() if x.is_dir()]
 
 
-def files(root, patterns=['*.mkv', '*.avi', '*.mp4', '*.srt']):
-    root = Path(root)
-    return chain(*[root.rglob(e) for e in patterns])
+def files(folder, patterns=['*.mkv', '*.avi', '*.mp4', '*.srt']):
+    return chain(*[Path(folder).glob(e) for e in patterns])
+
+
+def files_r(folder, patterns=['*.mkv', '*.avi', '*.mp4', '*.srt']):
+    return chain(*[Path(folder).rglob(e) for e in patterns])
 
 
 def files_media(root):
@@ -24,3 +25,11 @@ def files_media(root):
 
 def files_subs(root):
     return files(root, patterns=['*.srt'])
+
+
+def files_media_r(root):
+    return files_r(root, patterns=['*.mkv', '*.avi', '*.mp4'])
+
+
+def files_subs_r(root):
+    return files_r(root, patterns=['*.srt'])
